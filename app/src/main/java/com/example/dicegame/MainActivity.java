6eee6fae2db6fc88a5828af52e9616b05d6eba9a
Ernesto5111 <162698713+Ernesto5111@users.noreply.github.com>
2024-11-05 11:13:46 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/dicegame/MainActivity.java
+++ app/src/main/java/com/example/dicegame/MainActivity.java
@@ -1,77 +1,105 @@
 package com.example.dicegame;
 
+import android.graphics.Color;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.dicegame.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
-
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+    private Button button_rzut;
+    private Button button_reset;
+    private TextView rzut1;
+    private TextView rzut2;
+    private TextView rzut3;
+    private TextView rzut4;
+    private TextView rzut5;
+    private TextView tytul;
+    private TextView wynik_losowania;
+    private TextView wynik_gry;
+    private TextView liczba_rzutow;
+    private Random ra = new Random();
+    int wynik_Gry = 0;
+    int licznik = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        rzut1 = findViewById(R.id.rzut1);
+        rzut2 = findViewById(R.id.rzut2);
+        rzut3 = findViewById(R.id.rzut3);
+        rzut4 = findViewById(R.id.rzut4);
+        rzut5 = findViewById(R.id.rzut5);
+        tytul = findViewById(R.id.tytul);
+        wynik_gry = findViewById(R.id.wynik_gry);
+        wynik_losowania = findViewById(R.id.wynik_losowania);
+        liczba_rzutow = findViewById(R.id.liczba_rzutow);
+        button_rzut = findViewById(R.id.button_rzut);
+        button_reset = findViewById(R.id.button_reset);
 
-        setSupportActionBar(binding.toolbar);
-
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
-        binding.fab.setOnClickListener(new View.OnClickListener() {
+        button_rzut.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAnchorView(R.id.fab)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                int rad1 = ra.nextInt(6) + 1;
+                rzut1.setText(Integer.toString(rad1));
+                int rad2 = ra.nextInt(6) + 1;
+                rzut2.setText(Integer.toString(rad2));
+                int rad3 = ra.nextInt(6) + 1;
+                rzut3.setText(Integer.toString(rad3));
+                int rad4 = ra.nextInt(6) + 1;
+                rzut4.setText(Integer.toString(rad4));
+                int rad5 = ra.nextInt(6) + 1;
+                rzut5.setText(Integer.toString(rad5));
+
+
+                int[] liczby = {rad1, rad2, rad3, rad4, rad5};
+                int wynik = 0;
+                int[] count = new int[6]; // Assuming the numbers are between 1 and 6
+
+                // Count the occurrences of each number
+                for (int i = 0; i < liczby.length; i++) {
+                    count[liczby[i] - 1]++;
+                }
+
+                // Calculate the score based on the occurrences
+                for (int i = 0; i < count.length; i++) {
+                    if (count[i] > 1) {
+                        wynik += (i + 1) * count[i];
+                    }
+                }
+
+                wynik_losowania.setText("Wynik tego losowania: " + wynik);
+                licznik++;
+                liczba_rzutow.setText("Liczba rzutów: " + licznik);
+                updateScore(wynik);
+
             }
         });
+
+        button_reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                rzut1.setText("?");
+                rzut2.setText("?");
+                rzut3.setText("?");
+                rzut4.setText("?");
+                rzut5.setText("?");
+                wynik_Gry = 0;
+                licznik = 0;
+                wynik_gry.setText("Wynik gry: " + wynik_Gry);
+                liczba_rzutow.setText("Liczba rzutów: " + licznik);
+            }
+        });
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
+    private void updateScore(int wynik) {
+        wynik_Gry +=wynik;
+        wynik_gry.setText("Wynik gry: " + wynik_Gry);
     }
 }
