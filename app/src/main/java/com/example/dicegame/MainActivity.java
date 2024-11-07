4f068a594b8f8fccb8ad38b23df3292e06bd1d8d
Ernesto5111 <162698713+Ernesto5111@users.noreply.github.com>
2024-11-07 18:29:41 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/dicegame/MainActivity.java
+++ app/src/main/java/com/example/dicegame/MainActivity.java
@@ -12,16 +12,16 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     private Button button_rzut;
     private Button button_reset;
-    private TextView rzut1;
-    private TextView rzut2;
-    private TextView rzut3;
-    private TextView rzut4;
-    private TextView rzut5;
+    private TextView rzut_1;
+    private TextView rzut_2;
+    private TextView rzut_3;
+    private TextView rzut_4;
+    private TextView rzut_5;
     private TextView tytul;
     private TextView wynik_losowania;
     private TextView wynik_gry;
     private TextView liczba_rzutow;
-    private Random ra = new Random();
+    private Random l = new Random();
     int wynik_Gry = 0;
     int licznik = 0;
 
@@ -30,11 +30,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        rzut1 = findViewById(R.id.rzut1);
-        rzut2 = findViewById(R.id.rzut2);
-        rzut3 = findViewById(R.id.rzut3);
-        rzut4 = findViewById(R.id.rzut4);
-        rzut5 = findViewById(R.id.rzut5);
+        rzut_1 = findViewById(R.id.rzut1);
+        rzut_2 = findViewById(R.id.rzut2);
+        rzut_3 = findViewById(R.id.rzut3);
+        rzut_4 = findViewById(R.id.rzut4);
+        rzut_5 = findViewById(R.id.rzut5);
         tytul = findViewById(R.id.tytul);
         wynik_gry = findViewById(R.id.wynik_gry);
         wynik_losowania = findViewById(R.id.wynik_losowania);
@@ -45,28 +45,28 @@ public class MainActivity extends AppCompatActivity {
         button_rzut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int rad1 = ra.nextInt(6) + 1;
-                rzut1.setText(Integer.toString(rad1));
-                int rad2 = ra.nextInt(6) + 1;
-                rzut2.setText(Integer.toString(rad2));
-                int rad3 = ra.nextInt(6) + 1;
-                rzut3.setText(Integer.toString(rad3));
-                int rad4 = ra.nextInt(6) + 1;
-                rzut4.setText(Integer.toString(rad4));
-                int rad5 = ra.nextInt(6) + 1;
-                rzut5.setText(Integer.toString(rad5));
+                int rad1 = l.nextInt(6) + 1;
+                rzut_1.setText(Integer.toString(rad1));
+                int rad2 = l.nextInt(6) + 1;
+                rzut_2.setText(Integer.toString(rad2));
+                int rad3 = l.nextInt(6) + 1;
+                rzut_3.setText(Integer.toString(rad3));
+                int rad4 = l.nextInt(6) + 1;
+                rzut_4.setText(Integer.toString(rad4));
+                int rad5 = l.nextInt(6) + 1;
+                rzut_5.setText(Integer.toString(rad5));
 
 
                 int[] liczby = {rad1, rad2, rad3, rad4, rad5};
                 int wynik = 0;
-                int[] count = new int[6]; // Assuming the numbers are between 1 and 6
+                int[] count = new int[6]; // liczby od 1-6
 
-                // Count the occurrences of each number
+                //liczymy zdarzenia
                 for (int i = 0; i < liczby.length; i++) {
                     count[liczby[i] - 1]++;
                 }
 
-                // Calculate the score based on the occurrences
+                // liczymy wynik na podstawie zdarzeń
                 for (int i = 0; i < count.length; i++) {
                     if (count[i] > 1) {
                         wynik += (i + 1) * count[i];
@@ -84,11 +84,11 @@ public class MainActivity extends AppCompatActivity {
         button_reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                rzut1.setText("?");
-                rzut2.setText("?");
-                rzut3.setText("?");
-                rzut4.setText("?");
-                rzut5.setText("?");
+                rzut_1.setText("?");
+                rzut_2.setText("?");
+                rzut_3.setText("?");
+                rzut_4.setText("?");
+                rzut_5.setText("?");
                 wynik_Gry = 0;
                 licznik = 0;
                 wynik_gry.setText("Wynik gry: " + wynik_Gry);
